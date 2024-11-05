54f76640d58007c499fce8c875a5fd193690e6ba
Stephen Booth <me@sbooth.org>
2024-11-04 22:27:23 -0600

    Update to version 0.1.1 (#3)
    Add tvOS support

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
 	name: "sndfile-binary-xcframework",
 	platforms: [
-		.macOS(.v10_15),
-		.iOS(.v14),
+		.macOS(.v11),
+		.iOS(.v15),
+		.tvOS(.v15),
 	],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
@@ -22,7 +23,7 @@ let package = Package(
 		// Targets can depend on other targets in this package and products from dependencies.
 		.binaryTarget(
 			name: "sndfile",
-			url: "https://github.com/sbooth/sndfile-binary-xcframework/releases/download/0.1.0/sndfile.xcframework.zip",
-			checksum: "a158c0a9106e8dca35a32cd2d7262dbda5e5fc62c99c7751cc2af97303dce966"),
+			url: "https://github.com/sbooth/sndfile-binary-xcframework/releases/download/0.1.1/sndfile.xcframework.zip",
+			checksum: "201051b08a8af2d0ba4cd7ebc7e643733d3977205c2e3b9176ec993fe2f33de7"),
 	]
 )
